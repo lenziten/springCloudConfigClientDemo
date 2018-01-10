7101a7b22d5a0fbf23bf135d04bcecfdcbfa20ff
lenziten <lenziten@hotmail.com>
2018-01-10 10:55:27 +0800

    commit

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,21 +1,14 @@
 package com;
 
-import org.mybatis.spring.annotation.MapperScan;
-//import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@RestController
-@MapperScan("com.dao")
 @EnableAutoConfiguration//(exclude={MapperScannerConfigurer.class})
 //@SpringBootApplication
-@EnableConfigServer
 @ComponentScan
 public class Application implements EmbeddedServletContainerCustomizer{
 
