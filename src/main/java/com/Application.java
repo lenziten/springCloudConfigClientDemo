c9364a1388115cbefe638e5c9f0f9056746724f9
lenziten <lenziten@hotmail.com>
2018-01-12 18:09:19 +0800

    commit

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@EnableDiscoveryClient
 @EnableAutoConfiguration//(exclude={MapperScannerConfigurer.class})
 //@SpringBootApplication
 @ComponentScan
--- src/main/java/com/web/CloseController.java
+++ src/main/java/com/web/CloseController.java
@@ -3,11 +3,13 @@ package com.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RefreshScope
 @RestController
 @RequestMapping("/exit")
 public class CloseController {
@@ -18,9 +20,9 @@ public class CloseController {
     @Value("${from}")
     private String from;
     
-    @RequestMapping(value="/from")
+    @RequestMapping(value="/from",method=RequestMethod.GET)
     private String from(){
-    	return this.from;
+    	return "from " + this.from;
     }
     
     @RequestMapping("/shutDown")
